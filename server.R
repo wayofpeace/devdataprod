9a6af1d0f0b182081c9e968326ebbb2d0bff19a6
wayofpeace <deritisc@gmail.com>
2015-03-22 19:00:50 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,4 +1,9 @@
-# server.R
+# server.R for the EconChart app
+# the server app below takes the user input for the economic indicator mnemonic
+# it then pulls in the data from the Federal Reserve of St Louis website site
+# it calls function in the helper.R file if the user selects checkbox options
+#    the helpers.R file contains funtions to performan statistical computations (using helpers.R for organization) 
+# it creates the output plot to be displayed by calling the chartSeries function
 
 library(quantmod)
 source("helpers.R")
